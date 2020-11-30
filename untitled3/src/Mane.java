40802c73df7a1cac4f52bd1f837d23b6351b2ea3
Sergey Kolobkov <skolobkov@yandex.ru>
2020-11-30 22:02:16 +0300

    Initioin commit

---
+++ untitled3/src/Mane.java
@@ -0,0 +1,14 @@
+
+public class Mane {
+    public static void main(String[] args) {
+        int priceticket = 1300;
+        int onemile = 20;
+        int totalmile =priceticket/onemile;
+
+        System.out.println(totalmile);
+
+    }
+
+
+
+}
